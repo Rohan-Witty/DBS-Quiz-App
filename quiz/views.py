d49fc4fd9e52d2a25f81750115034aa8112d03b1
Rohan Srinivasan <nimbus.series@gmail.com>
2022-04-10 11:00:39 +0530

    Updated quiz.html
    1. Added submit button
    2. Display correct option after submission

--- quiz/views.py
+++ quiz/views.py
@@ -1,12 +1,13 @@
 from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
-from quiz.models import Option, Question
+from quiz.models import Option, Question, CorrectOption
 
 def home(request):
     return render(request, 'quiz/home.html')
 
 def quiz(request):
-    results = Question.objects.all()
-    res = Option.objects.all()
-    return render(request, 'quiz/quiz.html', {"Question": results, "Option": res})
+    questions = Question.objects.all()
+    options = Option.objects.all()
+    correct_options = CorrectOption.objects.all()
+    return render(request, 'quiz/quiz.html', {"Question": questions, "Option": options, "CorrectOption": correct_options})
